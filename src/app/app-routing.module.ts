097797b698e9ebddff136ae7cadb8ac6befc0b1d
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-06-07 15:50:37 +0200

    Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { LoginComponent } from './views/login/login.component';
+import { authenticatedGuard } from './guards/authenticated.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'accueil',
+        component: HomeComponent
+    },
+
+    {
+        path: 'connexion',
+        component: LoginComponent
+    },
+
+    {
+        path: '',
+        redirectTo: 'accueil',
+        pathMatch: 'full'
+    },
+
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { LoginComponent } from './views/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthenticationInterceptor } from './interceptors/authentication.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+        {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthenticationInterceptor,
+        multi: true
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
